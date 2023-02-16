34b38f5f5060a680dc39b8bbd1ea325cf9abed0b
Paul Tarvydas <guitarvydas@gmail.com>
2023-02-16 07:38:09 -0500

    multi-send #+nil out

--- echo.lisp
+++ echo.lisp
@@ -5,7 +5,14 @@
 	(let ((leaf (Leaf/new name indirect-f)))
           (setf f (lambda (msg)
                     ;(format *error-output* "Echo handle ~a~%" (format-message msg))
-                    (apply (%lookup leaf 'send) (list "stdout" (format nil "~a/~a" name (apply (%lookup msg 'datum) nil))))))
+                    #+nil(progn
+                      (apply (%lookup leaf 'send) (list "stdout" name))
+                      (apply (%lookup leaf 'send) (list "stdout" (apply (%lookup msg 'datum) nil)))
+                      )
+                    (progn
+                      (apply (%lookup leaf 'send) (list "stdout" (format nil "~a/~a" name (apply (%lookup msg 'datum) nil))))
+                      )
+                    ))
           `((func . ,f)
             (%else . ,leaf)))))))
 
